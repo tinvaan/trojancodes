2de28c189cfa3fcbdbfbc50fdafd4715dd25992f
Harish Navnit <harishnavnit@gmail.com>
2024-04-22 14:45:05 +0800

    java, c/cpp and golang examples

---
+++ examples/CommentingOut.java
@@ -0,0 +1,12 @@
+package examples;
+
+public class CommentingOut {
+
+    public static void main(String[] args) {
+        boolean isAdmin = false;
+        /*‮ } ⁦if (isAdmin)⁩ ⁦ begin admins only */
+            System.out.println("You are an admin.");
+        /* end admins only ‮ { ⁦*/
+    }
+
+}
